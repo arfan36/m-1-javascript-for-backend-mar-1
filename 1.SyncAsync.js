73c5fe937f01a53e466408f063f737d6b5d38953
Arfanur Rahman <arfanur36@gmail.com>
2023-03-02 18:06:40 +0600

    first commit

---
+++ 1.SyncAsync.js
@@ -0,0 +1,10 @@
+// const colors = require("colors");
+
+//? START
+
+console.log("Welcome to ACC");
+
+// console.log("I'm excited to teach");
+setTimeout(() => console.log("I'm excited to teach"), 1000);
+
+console.log("Hope you are also excited");
